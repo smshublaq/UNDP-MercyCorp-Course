d39fe797f68cb667f033daf968e06c434679f687
Saleem <saleemshublaq@gmail.com>
2017-11-19 11:51:45 +0200

    spinner

--- SpinnerExample/app/src/main/java/com/app/spinnerexample/MainActivity.java
+++ SpinnerExample/app/src/main/java/com/app/spinnerexample/MainActivity.java
@@ -2,6 +2,14 @@ package com.app.spinnerexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +17,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Spinner spinner = findViewById(R.id.mySpinner);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        /*
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>
+                        (this,R.layout.spinner_item
+                                ,R.id.tvItem);
+        */
+        String[] items = {"A","b","c"};
+        List<String> list = new ArrayList();
+        list.add("a");
+                list.add("b");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
+
+
+       adapter.add("ahmed");
+       adapter.add("mohammed");
+       adapter.add("hassan");
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long viewId) {
+                String s = (String)adapterView.getItemAtPosition(position);
+
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
