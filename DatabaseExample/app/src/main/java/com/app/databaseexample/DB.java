8316cd604905cd98180f1fbbb89a06337e5355fb
Saleem <saleemshublaq@gmail.com>
2017-11-30 12:12:34 +0200

    database

--- DatabaseExample/app/src/main/java/com/app/databaseexample/DB.java
+++ DatabaseExample/app/src/main/java/com/app/databaseexample/DB.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 
 /**
  * Created by iSaleem on 11/28/17.
@@ -29,24 +30,29 @@ public class DB {
     }
 
     public long addStudent(String name){
+        sqLiteDatabase.beginTransaction();
         ContentValues contentValues = new ContentValues();
         contentValues.put(MySqliteDatabaseHelper.STUDENT_NAME_COLUMN,name);
 
         long id = sqLiteDatabase.insert(MySqliteDatabaseHelper.STUDENT_TABLE
                 ,null,contentValues);
+        sqLiteDatabase.endTransaction();
         return id;
     }
 
     public Cursor getStudents(){
         Cursor cursor = sqLiteDatabase.rawQuery("select * from " +
                 MySqliteDatabaseHelper.STUDENT_TABLE ,null);
+
+        //Cursor cursor = sqLiteDatabase.query(MySqliteDatabaseHelper.STUDENT_TABLE,new String[]{MySqliteDatabaseHelper.STUDENT_NAME_COLUMN},null,null,null,null,null,null);
+
         return cursor;
     }
 
     public Cursor getStudents(int id){
         Cursor cursor = sqLiteDatabase.rawQuery("select * from " +
                 MySqliteDatabaseHelper.STUDENT_TABLE +
-                " WHERE id = ?  ",new String[]{id+""});
+                " WHERE id=? ",new String[]{id+""});
         return cursor;
     }
 
@@ -65,6 +71,13 @@ public class DB {
                 ,""
                 ,new String[]{id+""});
         return deletedRows;
+    }
+    
+    public long insertWithHighPerformance(String name){
+        SQLiteStatement sqLiteStatement = sqLiteDatabase.compileStatement("insert into " + MySqliteDatabaseHelper.STUDENT_TABLE + "("+MySqliteDatabaseHelper.STUDENT_NAME_COLUMN + " values(?)" );
+        sqLiteStatement.bindString(1,name);
+        long id = sqLiteStatement.executeInsert();
+        return id;
     }
 
 }
--- DatabaseExample/app/src/main/java/com/app/databaseexample/MainActivity.java
+++ DatabaseExample/app/src/main/java/com/app/databaseexample/MainActivity.java
@@ -1,10 +1,14 @@
 package com.app.databaseexample;
 
 import android.database.Cursor;
+import android.os.Environment;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -16,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView lst = findViewById(R.id.lst);
-
+        final EditText editText = findViewById(R.id.etName);
         DB db = new DB(this);
         db.open();
 
@@ -64,5 +68,25 @@ public class MainActivity extends AppCompatActivity {
         //simpleCursorAdapter.swapCursor();
         db.close();
 
+
+        lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                DB db = new DB(MainActivity.this);
+                db.open();
+                Cursor c = db.getStudents();
+                c.moveToPosition(position);
+                String name = c.getString(c.getColumnIndex(MySqliteDatabaseHelper.STUDENT_NAME_COLUMN));
+                int id = c.getInt(c.getColumnIndex(MySqliteDatabaseHelper.STUDENT_ID_COLUMN));
+                editText.setText(name + " : " + id);
+                db.close();
+            }
+        });
+
+        String directory = Environment.getExternalStorageDirectory().getPath();
+        Toast.makeText(this, directory, Toast.LENGTH_SHORT).show();
+
     }
+
+
 }
