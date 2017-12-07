a0f06e06597a8ffdb2a50fdfd8baa6a9ceadf2fd
Saleem <saleemshublaq@gmail.com>
2017-12-07 12:23:40 +0200

    mm

--- VolleyExample/app/src/main/java/com/app/volleyexample/MainActivity.java
+++ VolleyExample/app/src/main/java/com/app/volleyexample/MainActivity.java
@@ -27,6 +27,48 @@ public class MainActivity extends AppCompatActivity {
         final RequestQueue requestQueue = Volley.newRequestQueue(this);
         final TextView tvJSON = findViewById(R.id.tvJSON);
 
-        
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(AppConstants.API_URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for(int i=0;i<response.length();i++){
+                    try {
+                        JSONObject jsonObject =  response.getJSONObject(i);
+                        int userId = jsonObject.getInt(AppConstants.USER_ID);
+                        int id = jsonObject.getInt(AppConstants.ID);
+                        String title = jsonObject.getString(AppConstants.TITLE);
+                        String body = jsonObject.getString(AppConstants.BODY);
+                        Post post = new Post(body,title,userId,id);
+
+                        tvJSON.append(post.getUserId()+"");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                "", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                //response.getJSONArray()
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+        requestQueue.add(jsonObjectRequest);
+
     }
 }
