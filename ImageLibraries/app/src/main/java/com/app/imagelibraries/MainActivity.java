6d5b3195a5135a252b203e638378114d54c96fac
Saleem <saleemshublaq@gmail.com>
2017-12-10 09:44:17 +0200

    a

--- ImageLibraries/app/src/main/java/com/app/imagelibraries/MainActivity.java
+++ ImageLibraries/app/src/main/java/com/app/imagelibraries/MainActivity.java
@@ -1,9 +1,15 @@
 package com.app.imagelibraries;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.android.volley.toolbox.NetworkImageView;
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NetworkImageView networkImageView = findViewById(R.id.img);
+        ImageView img = findViewById(R.id.img);
 
 
+        /*
         networkImageView.setImageUrl
                 ("https://vignette.wikia.nocookie.net/pokemon/images/1/13/007Squirtle_Pokemon_Mystery_Dungeon_Explorers_of_Sky.png/revision/latest?cb=20150105230449"
                         ,MyApplication.getInstance().getImageLoader());
+        */
+        /*
+        Picasso.with(this).load("https://vignette.wikia.nocookie.net/pokemon/images/1/13/007Squirtle_Pokemon_Mystery_Dungeon_Explorers_of_Sky.png/revision/latest?cb=20150105230449")
+
+                .into(new Target() {
+                    @Override
+                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+
+                    }
+
+                    @Override
+                    public void onBitmapFailed(Drawable errorDrawable) {
+
+                    }
+
+                    @Override
+                    public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                    }
+                });
+        */
+        Glide.with(this).load("https://vignette.wikia.nocookie.net/pokemon/images/1/13/007Squirtle_Pokemon_Mystery_Dungeon_Explorers_of_Sky.png/revision/latest?cb=20150105230449").into(img
+        );
 
     }
 }
